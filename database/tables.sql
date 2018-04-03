56f7d09741d5f33a6c9c603a210919a5c5577b24
Артём <34057772+dofyk@users.noreply.github.com>
2018-04-02 18:02:41 -0700

    Upade table.sql
    Add additional information, edits

--- database/tables.sql
+++ database/tables.sql
@@ -95,7 +95,7 @@ INSERT INTO Roles VALUES ('U', 'User');
 
 INSERT INTO sys_user (langara_id, first_name, last_name, email, upassword, salt, role_id) VALUES (100290512, 'Elian', 'Figueiredo', 'elian_gaspar@hotmail.com', '3ed8af9cacf92919bc34f4a034ac16140e1e3d349f199e94533f42acc630e3732c2c673ae4bdfe1173328132cd2ac774c010cb2fd6a7794c49a46dd90d3da110', '3cfce133c67eb6a7117d75fbcd8e3aa110f7b1c9d49c9916789641e327bf73387267f832160cfae2321ba3fc236f901e95ace8e44534e8e14bc8fa5324302d0c', 'U');
 INSERT INTO sys_user (langara_id, first_name, last_name, email, upassword, salt, role_id) VALUES (100302918, 'Caue', 'Pinheiro', 'cauehier@gmail.com', '3ed8af9cacf92919bc34f4a034ac16140e1e3d349f199e94533f42acc630e3732c2c673ae4bdfe1173328132cd2ac774c010cb2fd6a7794c49a46dd90d3da110', '3cfce133c67eb6a7117d75fbcd8e3aa110f7b1c9d49c9916789641e327bf73387267f832160cfae2321ba3fc236f901e95ace8e44534e8e14bc8fa5324302d0c', 'U');
-INSERT INTO sys_user (langara_id, first_name, last_name, email, upassword, salt, role_id) VALUES (100288803, 'Artyom', 'Lunyakin', 'artyomluu@gmail.com', '3ed8af9cacf92919bc34f4a034ac16140e1e3d349f199e94533f42acc630e3732c2c673ae4bdfe1173328132cd2ac774c010cb2fd6a7794c49a46dd90d3da110', '3cfce133c67eb6a7117d75fbcd8e3aa110f7b1c9d49c9916789641e327bf73387267f832160cfae2321ba3fc236f901e95ace8e44534e8e14bc8fa5324302d0c', 'U');
+INSERT INTO sys_user (langara_id, first_name, last_name, email, upassword, salt, role_id) VALUES (100288803, 'Artjoms', 'Lunakins', 'artyomluu@gmail.com', '3ed8af9cacf92919bc34f4a034ac16140e1e3d349f199e94533f42acc630e3732c2c673ae4bdfe1173328132cd2ac774c010cb2fd6a7794c49a46dd90d3da110', '3cfce133c67eb6a7117d75fbcd8e3aa110f7b1c9d49c9916789641e327bf73387267f832160cfae2321ba3fc236f901e95ace8e44534e8e14bc8fa5324302d0c', 'U');
 INSERT INTO sys_user (langara_id, first_name, last_name, email, upassword, salt, role_id) VALUES (100290513, 'Admin', 'Elian', 'elian.fig@gmail.com', '3ed8af9cacf92919bc34f4a034ac16140e1e3d349f199e94533f42acc630e3732c2c673ae4bdfe1173328132cd2ac774c010cb2fd6a7794c49a46dd90d3da110', '3cfce133c67eb6a7117d75fbcd8e3aa110f7b1c9d49c9916789641e327bf73387267f832160cfae2321ba3fc236f901e95ace8e44534e8e14bc8fa5324302d0c', 'A');
 INSERT INTO sys_user (langara_id, first_name, last_name, email, upassword, salt, role_id) VALUES (100578901, 'John', 'Doe', 'john.doe@email.com', '3ed8af9cacf92919bc34f4a034ac16140e1e3d349f199e94533f42acc630e3732c2c673ae4bdfe1173328132cd2ac774c010cb2fd6a7794c49a46dd90d3da110', '3cfce133c67eb6a7117d75fbcd8e3aa110f7b1c9d49c9916789641e327bf73387267f832160cfae2321ba3fc236f901e95ace8e44534e8e14bc8fa5324302d0c', 'U');
 INSERT INTO sys_user (langara_id, first_name, last_name, email, upassword, salt, role_id) VALUES (100290147, 'Abby', 'Morssy', 'abbymorssy@email.com', '3ed8af9cacf92919bc34f4a034ac16140e1e3d349f199e94533f42acc630e3732c2c673ae4bdfe1173328132cd2ac774c010cb2fd6a7794c49a46dd90d3da110', '3cfce133c67eb6a7117d75fbcd8e3aa110f7b1c9d49c9916789641e327bf73387267f832160cfae2321ba3fc236f901e95ace8e44534e8e14bc8fa5324302d0c', 'U');
@@ -114,8 +114,9 @@ INSERT INTO student (langara_id, major) VALUES ('100578901', 'Nursing');
 INSERT INTO staff (langara_id, position) VALUES ('100290147', 'Accountant');
 INSERT INTO staff (langara_id, position) VALUES ('100302681', 'Technician');
 INSERT INTO staff (langara_id, position) VALUES ('100288248', 'Delivery');
-INSERT INTO staff (langara_id, position) VALUES ('100290997', 'Teacher');
+INSERT INTO staff (langara_id, position) VALUES ('100290997', 'Instructor');
 INSERT INTO staff (langara_id, position) VALUES ('100578485', 'Nurse');
+INSERT INTO staff (langara_id, position) VALUES ('100991234', 'Advisor');
 INSERT INTO staff (langara_id, position) VALUES ('100123456', 'Teaching Assistant');
 
 INSERT INTO book (isbn13, title, edition, author) VALUES (9780134322759, 'Web Development & Design Foundations With HTML5', '8th Edition', 'Terry Felke-Morris');
